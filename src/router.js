b4c56105e95ca60ff7a90f5dd204c5f2a2c27afc
hasansaad1 <hasansaad.saad1998@gmail.com>
2017-11-28 16:57:00 +0200

    refactoring the back-end code
    based on issue #11

--- src/router.js
+++ src/router.js
@@ -1,19 +1,24 @@
-const {html, staticFiles, handleError} = require("./handler.js");
+const {home, staticFiles,handleRequest, handleError} = require("./handler.js");
 
-const routes = {
-  "/" : html,
-  "/style.css" : staticFiles,
-  "/main.js" : staticFiles,
-  "404" : handleError
-}
+// const routes = {
+//   "/" : home,
+//   "/style.css" : staticFiles,
+//   "/main.js" : staticFiles,
+//   "404" : handleError,
+//   ""
+// }
 
 
 function router (request, response) {
-  if(routes[request.url]) {
-    routes[request.url](request, response, request.url);
+  let baseUrl = request.url;
+  if(baseUrl==="/") {
+    home(request, response);
+  }else if(url.pathname==='/submit'){
+    handleRequest(request,response);
   }
-  else {
-    routes[404]("404, page not found", request, response);
+
+  else{
+    staticFiles(request, response);
   }
 }
 
