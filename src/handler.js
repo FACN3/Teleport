1349f44059a1076ff9252f2ea23b54bfbf36b971
MynahMarie <mynah.marie@gmail.com>
2017-11-30 10:03:11 +0200

    Start correcting with eslint
    Relates #45

--- src/handler.js
+++ src/handler.js
@@ -2,76 +2,91 @@ const fs = require('fs');
 const path = require('path');
 const req = require('request');
 
-//ogject that holds the data recieved
+// object that holds the data recieved
 const dataObj = {
-  'clock':"",
-  'weather': ""
+  clock: '',
+  weather: '',
+};
+
+// filters the data recieved by both apis
+function filterData(body, num) {
+  // const parsedBody = JSON.parse(body);
+
+  if (num === 0) {
+    dataObj.clock = body;
+  }
+  if (num === 1) {
+    dataObj.weather = body;
+  }
 }
 
-//handles the home route - /
+// handles the Errors
+function handleError(error, request, response) {
+  response.writeHead(404, { 'Content-Type': 'text/html' });
+  response.end('<h1>404 PAGE NOT FOUND </h1>');
+}
+
+// handles the home route - /
 function handleHome(request, response) {
-  let filePath =path.join(__dirname,'..','public','index.html')  ;
+  const filePath = path.join(__dirname, '..', 'public', 'index.html');
   // handler.staticFiles (request, response, file);
   fs.readFile(filePath, (error, file) => {
-    if(error) {
+    if (error) {
       handleError(error, request, response);
     } else {
-    response.writeHead(200, {"Context-Type" : 'text/html'});
-    response.end(file);
-    }
-  });
-}
-
-//handles the rest of the public files
-function handleStatic(request, response) {
-
-  let filePath = path.join(__dirname, '..', 'public', request.url);
-  const extension = request.url.split(".")[1];
-  const extensionType = {
-    html : "text/html",
-    css : "text/css",
-    js : "application/javascript"
-  }[extension];
-
-  fs.readFile(filePath, (error, file) => {
-    if(error) {
-      handleError(error, request, response);
-    }else{
-      response.writeHead(200, {"Context-Type" : extensionType});
+      response.writeHead(200, { 'Context-Type': 'text/html' });
       response.end(file);
     }
   });
-};
+}
 
-//send an API request to other server
-function handleRequest(request , response){
-  // var parallel =0;
-  let query = request.url.split('=')[1];
-  let weather_url = 'https://api.openweathermap.org/data/2.5/weather?q=' + query + '&appid=82e45ca76afa605e31bf2540d2afe634';
+// handles the rest of the public files
+function handleStatic(request, response) {
+  const filePath = path.join(__dirname, '..', 'public', request.url);
+  const extension = request.url.split('.')[1];
+  const extensionType = {
+    html: 'text/html',
+    css: 'text/css',
+    js: 'application/javascript',
+  }[extension];
 
-  req(weather_url, (error, res, body) => {
-      // console.log("ERROR: ",error);
+  fs.readFile(filePath, (error, file) => {
     if (error) {
-      console.log("error wuth the recieved data : ", error.message);
       handleError(error, request, response);
     } else {
-      console.log("WEATHER API RESPONSE:  ",body);
-      filterData(body,1);
+      response.writeHead(200, { 'Context-Type': extensionType });
+      response.end(file);
+    }
+  });
+}
 
-      //sending the second api
-      body = JSON.parse(body);
-      let lat = body.coord.lat;
-      let long = body.coord.lon;
-      let time_url = "http://api.timezonedb.com/v2/get-time-zone?key=UOEG6CXIM1AQ&format=json&by=position&lat=" + lat + "&lng=" + long;
+// send an API request to other server
+function handleRequest(request, response) {
+  const query = request.url.split('=')[1];
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${
+    query
+  }&appid=82e45ca76afa605e31bf2540d2afe634`;
 
-      req(time_url, (error, res, body) => {
-        if (error) {
-          console.log("error wuth the recieved data : ", error.message);
-          handleError(error, request, response);
+  req(weatherUrl, (error, res, body) => {
+    if (error) {
+      handleError(error, request, response);
+    } else {
+      filterData(body, 1);
+
+      // sending the second api
+      const parsedBody = JSON.parse(body);
+      const { coord: { lon, lat } } = parsedBody;
+      // const long = parsedBody.coord.lon;
+      const timeUrl = `http://api.timezonedb.com/v2/get-time-zone?key=UOEG6CXIM1AQ&format=json&by=position&lat=${
+        lat
+      }&lng=${lon}`;
+
+      req(timeUrl, (err, result, timeBody) => {
+        if (err) {
+          handleError(err, request, response);
         } else {
-          console.log("TIME API RESPONSE:  ",body);
-          filterData(body,0);
-          response.writeHead(200, {'Content-Type': 'application/json'});
+          filterData(timeBody, 0);
+          response.writeHead(200, { 'Content-Type': 'application/json' });
           response.end(JSON.stringify(dataObj));
         }
       });
@@ -79,27 +94,9 @@ function handleRequest(request , response){
   });
 }
 
-//handles the Errors
-function handleError(error, request, response) {
-  response.writeHead(404, {"Content-Type" : "text/html"});
-  response.end("<h1>404 PAGE NOT FOUND </h1>");
-};
-
-//filters the data recieved by both apis
-function filterData(body,num){
-  body = JSON.parse(body);
-
-  if(num == 0){
-    dataObj['clock'] = JSON.stringify(body);
-  }
-  if(num == 1){
-    dataObj['weather'] = JSON.stringify(body);
-  }
-}
-
 module.exports = {
   handleHome,
   handleStatic,
   handleRequest,
-  handleError
+  handleError,
 };
